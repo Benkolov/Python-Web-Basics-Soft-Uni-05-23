c7ce08065b04e03eb3b2f67516ea9fb4a4d4ed02
Benkolov <111705203+Benkolov@users.noreply.github.com>
2023-06-01 14:26:34 +0300

    Initial commit

--- 04.URLs and Views/departments_project/departments_project/departments/urls.py
+++ 04.URLs and Views/departments_project/departments_project/departments/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from .views import old_view, new_view, department_by_slug_view
 
-from departments_project.departments.views import index
-
+# basic
 urlpatterns = [
-    path("", index),
+    path("old/", old_view, name='old page'),
+    path("new-page/", new_view, name='new page'),
+    # /departments/<slug>
+    path("<slug:department_name>/", department_by_slug_view, name="department by slug")
 ]
