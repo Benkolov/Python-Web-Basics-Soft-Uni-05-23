1d18868012d7216e552c83988172159f24018fb4
Benkolov <111705203+Benkolov@users.noreply.github.com>
2023-06-01 14:35:58 +0300

    Initial commit

--- 04.URLs and Views/departments_app/departments_app/departments/views.py
+++ 04.URLs and Views/departments_app/departments_app/departments/views.py
@@ -1,9 +1,25 @@
-from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render, redirect
+from random import choice
 
+from django.http import HttpResponse, HttpRequest, HttpResponseNotFound, HttpResponseBadRequest, Http404
+from django.shortcuts import render, redirect, get_object_or_404
+
+
+# Without render
+# def show_departments(request: HttpRequest, *args, **kwargs):
+#     print(request.method)
+#     print(request.GET)  # query params (?param1=value1&param2=value2
+#     print(request.POST)  # body of HTTP request
+#     print(request.get_port())
+#     print(request.get_host())
+#     print(request.headers)
+#
+#     order_by = request.GET.get('order_by', 'name')
+#     body = f'path: {request.path}, args={args}, kwargs={kwargs}, order_by: {order_by}'
+#     return HttpResponse(body)
 
 def show_departments(request: HttpRequest, *args, **kwargs):
     context = {
+        'page_title': 'Departments',
         'method': request.method,
         'order_by': request.GET.get('order_by', 'name'),
     }
@@ -11,6 +27,26 @@ def show_departments(request: HttpRequest, *args, **kwargs):
     return render(request, 'departments/all.html', context)
 
 
-def show_departments_details(request, departments_id):
-    body = f'path: {request.path}, id:{departments_id}'
+def show_department_details(request: HttpRequest, department_id):
+    body = f'path: {request.path}, id: {department_id}'
     return HttpResponse(body)
+
+
+def redirect_to_first_department(request):
+    possible_order_by = ['name', 'age', 'id']
+    order_by = choice(possible_order_by)
+
+    # to = 'https://softuni.bg'
+    # to = f'/departments/?order_by={order_by}'
+    return redirect('show department details', department_id=13)
+
+
+def show_not_found(request):
+    # # status_code = 400
+    # print('With response')
+    # # return HttpResponseNotFound('This not found')
+    # # return HttpResponse("Error", status=status_code)
+    # raise Http404('Not found!')
+
+    values = []
+    return HttpResponse(values[0])
--- 04.URLs and Views/departments_app/departments_app/urls.py
+++ 04.URLs and Views/departments_app/departments_app/urls.py
@@ -1,10 +1,12 @@
+# Project urls.py
+
 from django.contrib import admin
 from django.urls import path, include
 
-from departments_app import departments
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('departments/',
-         include('departments_app.departments.urls'))
+    path(
+        'departments/',
+        include('departments_app.departments.urls')),
 ]
+
