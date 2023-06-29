de0497195dcd3b7a819e5a28a1d98170237bc064
Benkolov <111705203+Benkolov@users.noreply.github.com>
2023-06-29 21:29:01 +0300

    Changes

---
+++ 08.Workshop/Petstagram/Petstagram/photos/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from Petstagram.photos.models import Photo
+
+
+def photo_add(request):
+    return render(request, 'photos/photo-add-page.html')
+
+
+def photo_details(request, pk):
+    photo = Photo.objects.get(pk=pk)
+
+    context = {
+        'photo': photo,
+        'likes': photo.like_set.count(),
+        'comments': photo.comment_set.all(),
+    }
+    return render(request, 'photos/photo-details-page.html', context=context)
+
+
+def photo_edit(request, pk):
+    return render(request, 'photos/photo-edit-page.html')
